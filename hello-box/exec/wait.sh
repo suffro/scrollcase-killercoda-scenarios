d4a328475a9cb301ffcc667aef26f512965f9fd4
Lorenzo S. <46894435+Suffro@users.noreply.github.com>
2026-08-10 05:03:25 +0200

    Update wait.sh

--- hello-box/exec/wait.sh
+++ hello-box/exec/wait.sh
@@ -9,14 +9,14 @@ i=0
 
 while true; do
   if [[ -f "$READY" ]]; then
-    printf "\rPreparing Scrollcase: ✓ Demo ready"
+    printf "\r Preparing Scrollcase: ✓ Demo ready"
     echo
     echo
     exit 0
   fi
 
   if [[ -f "$FAILED" ]]; then
-    printf "\rPreparing Scrollcase: ✗ Demo setup failed"
+    printf "\r Preparing Scrollcase: ✗ Demo setup failed"
     echo
     echo
     exit 1
@@ -30,7 +30,7 @@ while true; do
 
   dots=$((i % 4))
 
-  printf "\rPreparing Scrollcase demo%-3s" "$(printf '%*s' "$dots" '' | tr ' ' '.')"
+  printf "\r Preparing Scrollcase demo%-3s" "$(printf '%*s' "$dots" '' | tr ' ' '.')"
 
   i=$((i + 1))
   sleep 1
