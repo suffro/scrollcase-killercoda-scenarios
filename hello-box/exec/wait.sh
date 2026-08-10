407d5ad6b494b8c1a145596bb24c5151d9deba2b
Lorenzo S. <46894435+Suffro@users.noreply.github.com>
2026-08-10 04:49:28 +0200

    Update wait.sh

--- hello-box/exec/wait.sh
+++ hello-box/exec/wait.sh
@@ -18,11 +18,13 @@ while true; do
     exit 1
   fi
   
-  if [[ i>0 ]]; then
-    dots=$((i % 4))
-
-    printf "\r\n\nPreparing Scrollcase demo%-3s" "$(printf '%*s' "$dots" '' | tr ' ' '.')\n\n"
+  if [[ i<=0 ]]; then
+    printf " "
   fi
+
+  dots=$((i % 4))
+
+  printf "\rPreparing Scrollcase demo%-3s" "$(printf '%*s' "$dots" '' | tr ' ' '.')"
 
   i=$((i + 1))
   sleep 1
