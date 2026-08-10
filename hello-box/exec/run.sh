456663805897b64f9cfcde3edce2bfd61736d848
Lorenzo S. <46894435+Suffro@users.noreply.github.com>
2026-08-10 05:22:04 +0200

    minor

--- hello-box/exec/run.sh
+++ hello-box/exec/run.sh
@@ -2,5 +2,11 @@
 
 cd ~/demo
 
+clear
+
+printf "\r Running hello-box"
+
 scrollcase run box/*.release.json \
   --public-key keys/example-signing-public.json
+
+printf "\r Running hello-box: done"
--- hello-box/exec/verify.sh
+++ hello-box/exec/verify.sh
@@ -2,5 +2,11 @@
 
 cd ~/demo
 
+clear
+
+printf "\r Verifing"
+
 scrollcase verify box/*.release.json \
   --public-key keys/example-signing-public.json
+
+printf "\r Verifing: done"
